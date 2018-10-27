3d7fde45121987729e7d0df7a9790276d8881c88
Peter Friedland <peter@mansfield-analytics.com>
2018-10-27 19:01:36 -0400

    added additional query parameter logic for time-based search results and other google searches including images, video, news, books.  Not Google Maps, however

--- server.js
+++ server.js
@@ -9,37 +9,48 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 app.get('*', (req, response) => {
-	console.log("request path=" + JSON.stringify(req.path))
+		console.log("request path=" + JSON.stringify(req.path))
     console.log("request parameters=" + JSON.stringify(req.params))
     console.log("request query=" + JSON.stringify(req.query))
-  	var path = '/search?q='+req.path.substr(1)
+
+
+
   	if (req.path === '/url' && req.query.q != null) {
   		// this is a google search result
   		// redirect to req.query.q
-  		response.redirect(req.query.q)
+  		response.redirect(req.query.q);
 
   	}
   	else {
-	   	var host = 'https://www.google.com'
+			var path = '/search?q='+req.path.substr(1);
+	   	var host = 'https://www.google.com';
 
-		if ((req.path === '/search' || req.path === '/advanced_search') 
-			&& req.query.q != null) {
-		  	// this is an embedded search
-		  	path = '/search?q=' + req.query.q
-		  	// check to see if this is a pagination
-		  	if (req.query.start != null) {
-		  		path = path + '&start=' + req.query.start
-		  	}
-		}	   	
-	 	console.log('google search path=' + path)
-
-		request(host+path, 
-			(error, resp, body) => 
-		{
-		    response.send(body)
-		});
+			if ((req.path === '/search' || req.path === '/advanced_search')
+				&& req.query.q != null) {
+			  	// this is an embedded search
+			  	path = '/search?q=' + req.query.q
+			  	// check to see if this is a pagination
+			  	if (req.query.start != null) {
+			  		path = path + '&start=' + req.query.start
+			  	}
+					// is this is time-based search?
+					else if (req.query.tbs != null) {
+						path = path + '&tbs=' + req.query.tbs
+					}
+					// is this is a images, video, news, shopping, books query?
+					if (req.query.tbm != null ){
+						path = path + '&tbm=' + req.query.tbm
+					}
+			}
+		 	console.log('google search path=' + path)
+			// make the search request to www.google.com
+			request(host+path,
+				(error, resp, body) =>
+			{
+			    response.send(body)
+			});
 
   	}
 
