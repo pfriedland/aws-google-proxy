b84c2475c433549c1910b41fdfdabdeb3bb5652d
Peter Friedland <peter@mansfield-analytics.com>
2018-10-26 21:31:07 -0400

    differentiate if this is an embedded google search, a related URL callup, or a pagination

--- server.js
+++ server.js
@@ -11,27 +11,29 @@ const HOST = '0.0.0.0';
 const app = express();
 app.set('view engine', 'ejs');
 app.get('*', (req, response) => {
-  // res.send('Hello world\n');
-  // console.log(req.path)
-
+	console.log("request path=" + JSON.stringify(req.path))
+    console.log("request parameters=" + JSON.stringify(req.params))
+    console.log("request query=" + JSON.stringify(req.query))
+  	var path = '/search?q='+req.path.substr(1)
   	if (req.path === '/url' && req.query.q != null) {
+  		// this is a google search result
   		// redirect to req.query.q
   		response.redirect(req.query.q)
+
   	}
   	else {
 	   	var host = 'https://www.google.com'
-	 	var path = '/search?q='+req.path.substr(1)
 
-		// var optionsget = {
-		//             headers: {
-		//                 'authority': host,
-		//                 'method': 'GET',
-		//                 'path': path,
-		//                 'Referer': 'https://www.google.com'
-		//             },
-		//             url: host+path,
-		//             method: 'GET'
-		//         };
+		if ((req.path === '/search' || req.path === '/advanced_search') 
+			&& req.query.q != null) {
+		  	// this is an embedded search
+		  	path = '/search?q=' + req.query.q
+		  	// check to see if this is a pagination
+		  	if (req.query.start != null) {
+		  		path = path + '&start=' + req.query.start
+		  	}
+		}	   	
+	 	console.log('google search path=' + path)
 
 		request(host+path, 
 			(error, resp, body) => 
@@ -40,10 +42,6 @@ app.get('*', (req, response) => {
 		});
 
   	}
-
-
-
-
 
 });
 
